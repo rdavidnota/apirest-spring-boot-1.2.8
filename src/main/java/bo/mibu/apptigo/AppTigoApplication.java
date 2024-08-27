702004cc169566204801793a97c7eb3642defac6
rdavi <rdavid.nota@gmail.com>
2024-08-27 01:40:44 -0400

    feat(PAQ-001): - enable jboss

--- src/main/java/bo/mibu/apptigo/AppTigoApplication.java
+++ src/main/java/bo/mibu/apptigo/AppTigoApplication.java
@@ -2,9 +2,16 @@ package bo.mibu.apptigo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AppTigoApplication {
+public class AppTigoApplication extends SpringBootServletInitializer{
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+        return builder.sources(AppTigoApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(AppTigoApplication.class, args);
     }
